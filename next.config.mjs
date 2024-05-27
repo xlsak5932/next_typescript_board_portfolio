09f0efdfeb885154a1b447adac625a9dc314481d
안교남 <skadldldl123@gmail.com>
2024-05-27 17:11:51 +0900

    next.config.mjs 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     typescript: {
-=======
-   typescript: {
->>>>>>> e1ec1b97cdca1d5d4970dcbd5899b64912f357a1
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
-      output: "export",
+      ignoreBuildErrors: true,
+    },
+    output: "export",     
 };
 
 export default nextConfig;
