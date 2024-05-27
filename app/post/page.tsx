25a4c0355d36f98df714420c4fdb9d4d624daeea
안교남 <skadldldl123@gmail.com>
2024-05-27 17:23:51 +0900

    post 폴더 삭제

--- app/post/page.tsx
+++
@@ -1,7 +0,0 @@
-import { fetchAllPost } from '@/lib/server_logic/lesson'
-
-export default async function Post() {
-  const allPost = await fetchAllPost()
-  console.log('allPost >>> ', allPost)
-  return <></>
-}
